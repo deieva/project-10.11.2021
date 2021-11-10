0d6b64407aa01b6b2d4d857e4613264ada5a052a
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-10 15:11:10 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Views/Home';
 
 function App() {
   return (
     <div>
-      
+       <Router>
+                {/* <Header /> */}
+                <Routes>
+                  
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+
+                    {/* <Route path="/articles">
+                        <Articles />
+                    </Route> */}
+
+                    {/* <Route>
+                        <Page404 />
+                    </Route> */}
+
+                </Routes>
+                {/* <Footer /> */}
+            </Router>
     </div>
   );
 }
