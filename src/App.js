42112365c5991d8cf6092a749b8edfbbd11967c2
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-10 17:17:27 +0200

    i bootstrap, add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Views/Home';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
        <Router>
-                {/* <Header /> */}
+                <Header />
                 <Routes>
 
                 <Route path="/" element={<Home />} />
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,52 +1,108 @@
+import '../Assets/Css/Home.css';
+import $ from 'jquery';
+
 function Home() {
+
+    function moveToSelected(element) {
+        if (element == "next") {
+          var selected = $(".selected").next();
+        } else if (element == "prev") {
+          var selected = $(".selected").prev();
+        } else {
+          var selected = element;
+        }
+      
+        var next = $(selected).next();
+        var prev = $(selected).prev();
+        var prevSecond = $(prev).prev();
+        var nextSecond = $(next).next();
+      
+        $(selected).removeClass().addClass("selected");
+      
+        $(prev).removeClass().addClass("prev");
+        $(next).removeClass().addClass("next");
+      
+        $(nextSecond).removeClass().addClass("nextRightSecond");
+        $(prevSecond).removeClass().addClass("prevLeftSecond");
+      
+        $(nextSecond).nextAll().removeClass().addClass("hideRight");
+        $(prevSecond).prevAll().removeClass().addClass("hideLeft");
+      }
+      
+      // Eventos teclado
+      $(document).keydown(function (e) {
+        switch (e.which) {
+          case 37: // left
+            moveToSelected("prev");
+            break;
+      
+          case 39: // right
+            moveToSelected("next");
+            break;
+      
+          default:
+            return;
+        }
+        e.preventDefault();
+      });
+      
+      $("#carousel div").click(function () {
+        moveToSelected($(this));
+      });
+      
+      $("#prev").click(function () {
+        moveToSelected("prev");
+      });
+      
+      $("#next").click(function () {
+        moveToSelected("next");
+      });
+      
 
     return (
 
         <div>
+            <main>
 
-<section>
-  <div class="container">
-    <div class="row justify-content-center m-0">
-      <div class="col-9 p-0">
-        <div class="form_wrapper position-relative">
-          {/* <!--left tab genrate--> */}
-          <div class="get_thumbnail">
-            <ul class="new_steps pichtProlist">
-              <li class="thumbnail disabled step_0 personalInfo move" data-index="0">Slide 1</li>
-            </ul>
-          </div>
-          {/* <!--end left tab genrate--> */}
-          <div class="form_box">
-            <ul class="sliders">
-              {/* <!--tab 1 start here--> */}
-              <li class="slide active">
-                <h1>slide 1</h1>
-              </li>
-              <li class="slide">
-                <h1>slide 2</h1>
-              </li>
-              <li class="slide">
-                <h1>slide 3</h1>
-              </li>
+<div id="carousel">
 
-              <li class="slide">
-                <h1>slide 4</h1>
-              </li>
-            </ul>
-          </div>
-          <div class="sideBarElement">
-            <ul class="steps pichtProlist">
-              <li class="thumbnail step_1" data-index="1">Slide 2</li>
-              <li class="thumbnail step_2" data-index="2">Slide 3</li>
-              <li class="thumbnail step_3" data-index="3">Slide 4</li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-    </div>
+  <div class="hideLeft">
+    <img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg"/>
   </div>
-</section>
+
+  <div class="prevLeftSecond">
+    <img src="https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg"/>
+  </div>
+
+  <div class="prev">
+    <img src="https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg"/>
+  </div>
+
+  <div class="selected">
+    <img src="https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg"/>
+  </div>
+
+  <div class="next">
+    <img src="https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg"/>
+  </div>
+
+  <div class="nextRightSecond">
+    <img src="https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg"/>
+  </div>
+
+  <div class="hideRight">
+    <img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg"/>
+  </div>
+
+</div>
+
+<div class="buttons">
+  <button id="prev">Prev</button>
+  <button id="next">Next</button>
+</div>
+
+</main>
+
 
         </div>
     )
