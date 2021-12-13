6c37b8ac8e7150211a720450b165fca45ed27189
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-12-13 09:59:52 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,54 +1,43 @@
 import '../Assets/Css/Header.css';
-
 function Header() {
-
-    return (
-
-        <div>
-          <container>
-
-<zoomer>Hover to Enter</zoomer>
-
-<secret>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida. Diam donec adipiscing tristique risus nec feugiat.
-  <clickable>FIND OUT HOW</clickable>
-</secret>
-
-<leftie><text>FIND US ON SOCIALS</text>
-  <icon class="bi bi-facebook"></icon>
-  <icon class="bi bi-twitter"></icon>
-  <icon class="bi bi-linkedin"></icon>
-  <icon class="bi bi-instagram"></icon>
-  <icon class="bi bi-discord"></icon>
-</leftie>
-
-<rightie>
-  <menu>
-    <menu-item>
-      WHO WE ARE
-    </menu-item>
-    <menu-item>
-      HOW WE CAN HELP
-    </menu-item>
-    <menu-item>
-      PROJECTS
-    </menu-item>
-    <menu-item>
-      HISTORY
-    </menu-item>
-    <menu-item>
-      TESTIMONIALS
-    </menu-item>
-    <menu-item>
-      GET IN TOUCH
-    </menu-item>
-  </menu>
-</rightie>
-
-</container>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <container>
+        <zoomer>Hover to Enter</zoomer>
+        <secret>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida. Diam donec adipiscing tristique risus nec feugiat.
+          <clickable>FIND OUT HOW</clickable>
+        </secret>
+        <leftie><text>FIND US ON SOCIALS</text>
+          <icon class="bi bi-facebook"></icon>
+          <icon class="bi bi-twitter"></icon>
+          <icon class="bi bi-linkedin"></icon>
+          <icon class="bi bi-instagram"></icon>
+          <icon class="bi bi-discord"></icon>
+        </leftie>
+        <rightie>
+          <menu>
+            <menu-item>
+              WHO WE ARE
+            </menu-item>
+            <menu-item>
+              HOW WE CAN HELP
+            </menu-item>
+            <menu-item>
+              PROJECTS
+            </menu-item>
+            <menu-item>
+              HISTORY
+            </menu-item>
+            <menu-item>
+              TESTIMONIALS
+            </menu-item>
+            <menu-item>
+              GET IN TOUCH
+            </menu-item>
+          </menu>
+        </rightie>
+      </container>
+    </div>
+  )
 }
-
 export default Header;
