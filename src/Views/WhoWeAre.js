c01a518c639f34f5afd79dd8e75fa62413a367f7
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-12-20 13:06:31 +0200

    Update WhoWeAre.js

--- src/Views/WhoWeAre.js
+++ src/Views/WhoWeAre.js
@@ -1,7 +1,43 @@
+import '../Assets/Css/WhoWeAre.css';
+
 function WhoWeAre() {
 
     return (
         <div>
+            <div className="boxes">
+                <div className="box">
+                    <div className="box__content">
+                        <p className="box__title">New York City</p>
+                        <p className="box__summary">
+                            New York City is the most populous city in the United States. With a 2020 population of 8,804,190 distributed over 300.46 square miles.
+                        </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box__content">
+                        <p className="box__title">Barcelona</p>
+                        <p className="box__summary">
+                            Barcelona is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia.
+                        </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box__content">
+                        <p className="box__title">London</p>
+                        <p className="box__summary">
+                            London is the capital and largest city of England and the United Kingdom. It stands on the River Thames in south-east England.
+                        </p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="box__content">
+                        <p className="box__title">Shanghai</p>
+                        <p className="box__summary">
+                            Shanghai is one of the four direct-administered municipalities of the People's Republic of China. The city is located on the southern estuary of the Yangtze River.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
