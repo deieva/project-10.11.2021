63335c6400eeab7d313fcbfcc2e5815e6c41d8e9
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-12-21 12:42:43 +0200

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,44 +1,44 @@
 import '../Assets/Css/Home.css';
 function Home() {
-  return (
-    <div>
-      <container>
-        <zoomer>Hover to Enter</zoomer>
-        <secret>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida. Diam donec adipiscing tristique risus nec feugiat.
-          <clickable>FIND OUT HOW</clickable>
-        </secret>
-        <leftie><text>FIND US ON SOCIALS</text>
-          <icon className="bi bi-facebook"></icon>
-          <icon className="bi bi-twitter"></icon>
-          <icon className="bi bi-linkedin"></icon>
-          <icon className="bi bi-instagram"></icon>
-          <icon className="bi bi-discord"></icon>
-        </leftie>
-        <rightie>
-          <menu>
-            <menu-item>
-              <a href='/whoweare'>WHO WE ARE</a>
-             
-            </menu-item>
-            <menu-item>
-              HOW WE CAN HELP
-            </menu-item>
-            <menu-item>
-              PROJECTS
-            </menu-item>
-            <menu-item>
-              HISTORY
-            </menu-item>
-            <menu-item>
-              TESTIMONIALS
-            </menu-item>
-            <menu-item>
-              GET IN TOUCH
-            </menu-item>
-          </menu>
-        </rightie>
-      </container>
-    </div>
-  )
+    return (
+        <div>
+            <container>
+                <zoomer>Hover to Enter</zoomer>
+                <secret>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida. Diam donec adipiscing tristique risus nec feugiat.
+                    <clickable>FIND OUT HOW</clickable>
+                </secret>
+                <leftie><text>FIND US ON SOCIALS</text>
+                    <icon className="bi bi-facebook"></icon>
+                    <icon className="bi bi-twitter"></icon>
+                    <icon className="bi bi-linkedin"></icon>
+                    <icon className="bi bi-instagram"></icon>
+                    <icon className="bi bi-discord"></icon>
+                </leftie>
+                <rightie>
+                    <menu>
+                        <menu-item>
+                            <a href='/whoweare'>WHO WE ARE</a>
+
+                        </menu-item>
+                        <menu-item>
+                            HOW WE CAN HELP
+                        </menu-item>
+                        <menu-item>
+                            PROJECTS
+                        </menu-item>
+                        <menu-item>
+                            HISTORY
+                        </menu-item>
+                        <menu-item>
+                            TESTIMONIALS
+                        </menu-item>
+                        <menu-item>
+                            GET IN TOUCH
+                        </menu-item>
+                    </menu>
+                </rightie>
+            </container>
+        </div>
+    )
 }
 export default Home;
