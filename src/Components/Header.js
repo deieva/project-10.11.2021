c4f1841c4e498b9e9c1fbb8ef4bc6634173886c1
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-12-31 13:34:20 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,30 +3,30 @@ function Header() {
 
     return (
         <div>
-            <header class="neumorphic-header">
-                <div class="navigation-container">
+            <header className="neumorphic-header">
+                <div className="navigation-container">
                     <input type="checkbox" name="navigation" id="navHandlerId" />
-                    <label class="header-menu-icon fa fa-bars" for="navHandlerId"></label>
-                    <nav id="headerNavigationId" class="header-navigation">
+                    <label className="header-menu-icon fa fa-bars" for="navHandlerId"></label>
+                    <nav id="headerNavigationId" className="header-navigation">
                         <input type="radio" name="headerNavigation" id="nav-1" checked />
-                        <label class="nav-item" for="nav-1" data="Codepen">
-                            <i class="nav-icon fa fa-codepen"></i>
+                        <label className="nav-item" for="nav-1" data="Codepen">
+                            <i className="nav-icon fa fa-codepen"></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-2" />
-                        <label class="nav-item" for="nav-2" data="Github">
-                            <i class="nav-icon fa fa-github"></i>
+                        <label className="nav-item" for="nav-2" data="Github">
+                            <i className="nav-icon fa fa-github"></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-3" />
-                        <label class="nav-item" for="nav-3" data="Linkedin">
-                            <i class="nav-icon fa fa-linkedin"></i>
+                        <label className="nav-item" for="nav-3" data="Linkedin">
+                            <i className="nav-icon fa fa-linkedin"></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-4" />
-                        <label class="nav-item" for="nav-4" data="Instagram">
-                            <i class="nav-icon fa fa-instagram"></i>
+                        <label className="nav-item" for="nav-4" data="Instagram">
+                            <i className="nav-icon fa fa-instagram"></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-5" />
-                        <label class="nav-item" for="nav-5" data="Facebook">
-                            <i class="nav-icon fa fa-facebook"></i>
+                        <label className="nav-item" for="nav-5" data="Facebook">
+                            <i className="nav-icon fa fa-facebook"></i>
                         </label>
                     </nav>
                 </div>
