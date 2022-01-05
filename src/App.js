37499aba182fe9c95ac15acbfd84ed048caafa27
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-01-05 23:25:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Footer';
 import WhoWeAre from './Views/WhoWeAre';
 import Header from './Components/Header';
 import HowWeCanHelp from './Views/HowWeCanHelp';
+import Projects from './Views/Projects';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/whoweare" element={<WhoWeAre />} />
                 <Route path="/howwecanhelp" element={<HowWeCanHelp />} />
+                <Route path="/projects" element={<Projects />} />
 
                 </Routes>
                 <Footer />
