695ed7066e001c258f1d0bdd66371ea0e0b33cec
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-01-11 12:19:20 +0200

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,28 +10,28 @@ function Header() {
                     <nav id="headerNavigationId" className="header-navigation">
 
                         <input type="radio" name="headerNavigation" id="nav-1" checked/>
-                        <label className="nav-item" for="nav-1" data="Home">
-                            <i className="nav-icon fa fa-home"></i>
+                        <label className="nav-item" for="nav-1" >
+                            <i className="nav-icon fa fa-home"><a href='/'>Home</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-2"/>
-                        <label className="nav-item" for="nav-2" data="Who we are">
-                            <i className="nav-icon fa fa-star"></i>
+                        <label className="nav-item" for="nav-2">
+                            <i className="nav-icon fa fa-star"><a href='/whoweare'>Who we are</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-3" />
-                        <label className="nav-item" for="nav-3" data="How we can help">
-                            <i className="nav-icon fa fa-heart"></i>
+                        <label className="nav-item" for="nav-3">
+                            <i className="nav-icon fa fa-heart"><a href='/howwecanhelp'>How we can help</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-4" />
-                        <label className="nav-item" for="nav-4" data="Projects">
-                            <i className="nav-icon fa fa-globe"></i>
+                        <label className="nav-item" for="nav-4">
+                            <i className="nav-icon fa fa-globe"><a href='/projects'>Projects</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-5" />
-                        <label className="nav-item" for="nav-5" data="History">
-                            <i className="nav-icon fa fa-map"></i>
+                        <label className="nav-item" for="nav-5" >
+                            <i className="nav-icon fa fa-map"><a href='/history'>History</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-6" />
-                        <label className="nav-item" for="nav-6" data="Testimonials">
-                            <i className="nav-icon fa fa-pencil"></i>
+                        <label className="nav-item" for="nav-6" >
+                            <i className="nav-icon fa fa-pencil"><a href='/testimonials'>Testimonials</a></i>
                         </label>
                         <input type="radio" name="headerNavigation" id="nav-7" />
                         <label className="nav-item" for="nav-7" data="Get in touch">
