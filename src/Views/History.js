7141d1ccc68f0c1383ce7777abcf679ba3cd9d54
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-01-20 12:14:41 +0200

    updates

--- src/Views/History.js
+++ src/Views/History.js
@@ -1,14 +1,25 @@
 import '../Assets/Css/History.css';
 
 function History() {
-
-
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+    
     return (
         <div>
             <div className="content">
-
                 <h2>History of markup languages</h2>
-
                 <button className="accordion">GenCode</button>
                 <div className="panel">
                     <p>The first well-known public presentation of markup languages in computer text processing was made by William W. Tunnicliffe at a conference in 1967, although he preferred to call it generic coding. It can be seen as a response to the emergence of programs such as RUNOFF that each used their own control notations, often specific to the target typesetting device. In the 1970s, Tunnicliffe led the development of a standard called GenCode for the publishing industry and later was the first chair of the International Organization for Standardization committee that created SGML, the first standard descriptive markup language. Book designer Stanley Rice published speculation along similar lines in 1970. Brian Reid, in his 1980 dissertation at Carnegie Mellon University, developed the theory and a working implementation of descriptive markup in actual use.</p>
@@ -16,17 +27,14 @@ function History() {
                     <p>In 1975, Goldfarb moved from Cambridge, Massachusetts to Silicon Valley and became a product planner at the IBM Almaden Research Center. There, he convinced IBM's executives to deploy GML commercially in 1978 as part of IBM's Document Composition Facility product, and it was widely used in business within a few years.</p>
                     <p>SGML, which was based on both GML and GenCode, was developed by Goldfarb in 1974.Goldfarb eventually became chair of the SGML committee. SGML was first released by ISO as the ISO 8879 standard in October 1986.</p>
                 </div>
-
                 <button className="accordion">troff and nroff</button>
                 <div className="panel">
                     <p>Some early examples of computer markup languages available outside the publishing industry can be found in typesetting tools on Unix systems such as troff and nroff. In these systems, formatting commands were inserted into the document text so that typesetting software could format the text according to the editor's specifications. It was a trial and error iterative process to get a document printed correctly. Availability of WYSIWYG ("what you see is what you get") publishing software supplanted much use of these languages among casual users, though serious publishing work still uses markup to specify the non-visual structure of texts, and WYSIWYG editors now usually save documents in a markup-language-based format.</p>
                 </div>
-
                 <button className="accordion">TeX</button>
                 <div className="panel">
                     <p>Another major publishing standard is TeX, created and refined by Donald Knuth in the 1970s and '80s. TeX concentrated on detailed layout of text and font descriptions to typeset mathematical books. This required Knuth to spend considerable time investigating the art of typesetting. TeX is mainly used in academia, where it is a de facto standard in many scientific disciplines. A TeX macro package known as LaTeX provides a descriptive markup system on top of TeX, and is widely used.</p>
                 </div>
-
                 <button className="accordion">Scribe, GML and SGML</button>
                 <div className="panel">
                     <p>The first language to make a clean distinction between structure and presentation was Scribe, developed by Brian Reid and described in his doctoral thesis in 1980.[10] Scribe was revolutionary in a number of ways, not least that it introduced the idea of styles separated from the marked up document, and of a grammar controlling the usage of descriptive elements. Scribe influenced the development of Generalized Markup Language (later SGML) and is a direct ancestor to HTML and LaTeX.</p>
@@ -35,7 +43,6 @@ function History() {
                     <p>SGML found wide acceptance and use in fields with very large-scale documentation requirements. However, many found it cumbersome and difficult to learn—a side effect of its design attempting to do too much and be too flexible. For example, SGML made end tags (or start-tags, or even both) optional in certain contexts, because its developers thought markup would be done manually by overworked support staff who would appreciate saving keystrokes.
                     </p>
                 </div>
-
                 <button className="accordion">HTML</button>
                 <div className="panel">
                     <p>In 1989, computer scientist Sir Tim Berners-Lee wrote a memo proposing an Internet-based hypertext system, then specified HTML and wrote the browser and server software in the last part of 1990. The first publicly available description of HTML was a document called "HTML Tags", first mentioned on the Internet by Berners-Lee in late 1991. It describes 18 elements comprising the initial, relatively simple design of HTML. Except for the hyperlink tag, these were strongly influenced by SGMLguid, an in-house SGML-based documentation format at CERN. Eleven of these elements still exist in HTML 4.</p>
@@ -53,10 +60,8 @@ function History() {
                     <p>Since January 2000, all W3C Recommendations for HTML have been based on XML rather than SGML, using the abbreviation XHTML (Extensible HyperText Markup Language). The language specification requires that XHTML Web documents must be well-formed XML documents. This allows for more rigorous and robust documents while using tags familiar from HTML.</p>
                     <p>One of the most noticeable differences between HTML and XHTML is the rule that all tags must be closed: empty HTML tags such as &lt; br &gt; must either be closed with a regular end-tag, or replaced by a special form: &lt; br &gt; (the space before the '/' on the end tag is optional, but frequently used because it enables some pre-XML Web browsers, and SGML parsers, to accept the tag). Another is that all attribute values in tags must be quoted. Finally, all tag and attribute names within the XHTML namespace must be lowercase to be valid. HTML, on the other hand, was case-insensitive.</p>
                 </div>
-
             </div>
         </div>
     )
 }
-
 export default History;
--- src/Views/HowWeCanHelp.js
+++ src/Views/HowWeCanHelp.js
@@ -4,37 +4,37 @@ function HowWeCanHelp() {
 
     return (
         <div>
-            <section class="services-area section-padding">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-7 col-md-12 wow fadeInUp" data-wow-delay="0.3s">
-                            <div class="section-title text-center">
+            <section className="services-area section-padding">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-7 col-md-12 wow fadeInUp" data-wow-delay="0.3s">
+                            <div className="section-title text-center">
                                 <h2>Our Services</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.4s">
-                            <div class="single-serviecs-block">
-                                <i class="fas fa-print"></i>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.4s">
+                            <div className="single-serviecs-block">
+                                <i className="fas fa-print"></i>
                                 <h3><a href="services-details.html">Digital Printing</a></h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.5s">
-                            <div class="single-serviecs-block">
-                                <i class="far fa-lightbulb"></i>
+                        <div className="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.5s">
+                            <div className="single-serviecs-block">
+                                <i className="far fa-lightbulb"></i>
                                 <h3><a href="services-details.html">Design Services</a></h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.6s">
-                            <div class="single-serviecs-block">
-                                <i class="far fa-envelope"></i>
+                        <div className="col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0.6s">
+                            <div className="single-serviecs-block">
+                                <i className="far fa-envelope"></i>
                                 <h3><a href="services-details.html">Mail Services</a></h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua.</p>
