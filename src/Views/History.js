7036f8aec6d4d4cafcf85f6ee4b48b96aca974d8
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-01-31 12:29:45 +0200

    using aos for history

--- src/Views/History.js
+++ src/Views/History.js
@@ -1,9 +1,28 @@
 import '../Assets/Css/History.css';
+import Aos from 'aos';
 
 function History() {
-
+  Aos.init()
   return (
     <div>
+      <center>
+        <h1>Historical events</h1>
+      </center>
+
+      <div className="timeline">
+        <div className="timeline-left">
+          <div className="card left" data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+        </div>
+        <div className="timeline-right">
+          <div className="card right" data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+        </div>
+        <div className="timeline-left">
+          <div className="card left" data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+        </div>
+        <div className="timeline-right">
+          <div className="card right" data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+        </div>
+      </div>
 
     </div>
   )
