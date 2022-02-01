8e4beef82670d45a421cadc98c063fbb6f36dd67
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-02-01 13:46:16 +0200

    Update History.js

--- src/Views/History.js
+++ src/Views/History.js
@@ -14,10 +14,10 @@ function History() {
           <div className="card left" data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
         </div>
         <div className="timeline-right">
-          <div className="card right" data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+          <div className="card right" data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore!</div>
         </div>
         <div className="timeline-left">
-          <div className="card left" data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
+          <div className="card left" data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore!</div>
         </div>
         <div className="timeline-right">
           <div className="card right" data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae reiciendis voluptates labore! Recusandae assumenda deleniti, ut sequi ratione rerum non corrupti illum nemo facere pariatur hic, eveniet libero vero!</div>
