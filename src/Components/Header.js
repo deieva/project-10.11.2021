5697910d033a1136ae220b7c888b3a8f4c2cf073
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-02-02 13:02:04 +0200

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,45 +3,23 @@ function Header() {
 
     return (
         <div>
-            <header className="neumorphic-header">
-                <div className="navigation-container">
-                    <input type="checkbox" name="navigation" id="navHandlerId" />
-                    <label className="header-menu-icon fa fa-bars" for="navHandlerId"></label>
-                    <nav id="headerNavigationId" className="header-navigation">
-
-                        <input type="radio" name="headerNavigation" id="nav-1" checked/>
-                        <label className="nav-item" for="nav-1" >
-                            <i className="nav-icon fa fa-home"><a href='/' className='header-links'>Home</a></i>
-                        </label>
-                        <input type="radio" name="headerNavigation" id="nav-2"/>
-                        <label className="nav-item" for="nav-2">
-                            <i className="nav-icon  fa fa-star"><a href='/whoweare' className='header-links'>Who we are</a></i>
-                        </label>
-                        <input type="radio" name="headerNavigation" id="nav-3" />
-                        <label className="nav-item" for="nav-3">
-                            <i className="nav-icon  fa fa-heart"><a href='/howwecanhelp' className='header-links'>How we can help</a></i>
-                        </label>
-                        <input type="radio" name="headerNavigation" id="nav-4" />
-                        <label className="nav-item" for="nav-4">
-                            <i className="nav-icon  fa fa-globe"><a href='/projects' className='header-links'>Projects</a></i>
-                        </label>
-                        <input type="radio" name="headerNavigation" id="nav-5" />
-                        <label className="nav-item" for="nav-5" >
-                            <i className="nav-icon fa fa-map"><a href='/history' className='header-links'>History</a></i>
-                        </label>
-                        <a href='/testimonials'>
-                        <input type="radio" name="headerNavigation" id="nav-6" />
-                        <label className="nav-item" for="nav-6" data="Testimonials">
-                            <i className="nav-icon fa fa-pencil"></i>
-                        </label>
-                        </a>
-                        <span href='/getintouch'>
-                        <input type="radio" name="headerNavigation" id="nav-7" />
-                        <label className="nav-item nav-icon" for="nav-7" data="Get in touch">
-                            <i className="nav-icon  fa fa-info"></i>
-                        </label>
-                        </span>
-                    </nav>
+            <header>
+                <a href="#" class="logo">
+                    <img src="https://1.bp.blogspot.com/-FSJpfNUfiwQ/YVmBlwUDTuI/AAAAAAAABA8/UJWwUNLmi8A-dbv_FAwXhnnOh93BsJiAgCLcBGAsYHQ/s320/logo2.png" />
+                    <h1>Online store</h1>
+                </a>
+                <input type="checkbox" id="check" />
+                <label for="check">
+                    <img src="https://1.bp.blogspot.com/-TQ6sqw09POE/YVmBmHDNLtI/AAAAAAAABBE/h2kA7cyP42gurgfw1OttA8o6-9IFWvm7wCLcBGAsYHQ/s0/menu1.png" class="img1" />
+                    <img src="https://1.bp.blogspot.com/-7FrNzRkSTfA/YVmBmBWPd1I/AAAAAAAABBA/7ddyu0cIswgmBH3FCqUAuOnCU6Rq5bgbgCLcBGAsYHQ/s0/menu2.png" class="img2" />
+                </label>
+                <div class="links">
+                    <div>
+                        <a href="#">Home</a>
+                        <a href="#">Content</a>
+                        <a href="#">Profile</a>
+                        <a href="#">About</a>
+                    </div>
                 </div>
             </header>
 
