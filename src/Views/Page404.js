3d1a5f3f45c54b6e4a2ae21484a802a9354d6684
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-02-08 22:17:40 +0200

    update page404

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,6 +1,7 @@
+import '../Assets/Css/Page404.css';
 function Page404() {
     return (
-        <div>
+        <div className='page404-container'>
             <p class="mega">4<span class="boom">0</span>4
                 <div class="bola">
                 </div>
