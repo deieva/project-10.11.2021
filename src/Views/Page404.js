a34b60f965b7bcf5a265b3db833bc1f2d71d661c
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-02-08 22:13:32 +0200

    Update Page404.js

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,6 +1,11 @@
 function Page404() {
     return (
         <div>
+            <p class="mega">4<span class="boom">0</span>4
+                <div class="bola">
+                </div>
+            </p>
+            <p class="mini">That's an error.</p>
         </div>
     )
 }
